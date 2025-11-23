5334b13f089bd6bfeffb4a498d52d03dd09fff96
avandeventer <vandeventerandy@gmail.com>
2025-11-22 19:55:55 -0500

    Adding PhaseType oops

---
+++ src/main/java/client/nowhere/model/PhaseType.java
@@ -0,0 +1,8 @@
+package client.nowhere.model;
+
+public enum PhaseType {
+    SUBMISSION, // Players submit and elaborate on text
+    VOTING,     // Players vote on final submissions
+    WINNING     // Winner display phase
+}
+
